566c0dde3fa95ceeb72c1088c6a8c8765b85ee9d
Zcyde <eldrinjoshpineda@gmail.com>
2025-12-07 14:03:07 +0800

    Home page fix then other pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Services } from './pages/services/services';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', component: Home, title: 'ScrubHub'
+    },
+    {
+        path:'services', component: Services, title: 'Services'
+    },
+];
