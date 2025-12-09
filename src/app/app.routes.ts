e0ca3136664f1bb67a4b435eb15caf0b52b66539
Edtech-bit <castilloed7899@gmail.com>
2025-12-10 00:26:20 +0800

    hi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { Services } from './pages/services/services';
 import { Contact } from './pages/contact/contact';
 import { Employee } from './pages/employee/employee';
 import { Appointment } from './pages/appointment/appointment';
+import { Error } from './pages/error/error';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home, title: 'ScrubHub' },
-  { path:'appointment',component: Appointment,title:'appointment'},
+  { path: 'appointment', component: Appointment, title: 'Appointment' },
   { path: 'contact', component: Contact, title: 'Contact' },
   { path: 'services', component: Services, title: 'Services' },
-  { path: 'employee', component: Employee, title: 'Employee' }
+  { path: 'employee', component: Employee, title: 'Employee' },
+  { path: '**', component: Error, title: '404 Not Found' }
 ];
